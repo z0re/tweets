836236ba8236e7e64dbe7fdf68beb8b6c45e0ae7
root <root@MININT-6S109JT.fareast.corp.microsoft.com>
2018-09-16 19:46:50 +0800

    Write to folder

---
+++ config.py
@@ -0,0 +1,6 @@
+#Get your Twitter API credentials and enter them here
+consumer_key = "o2l6kS4ePiVZVgnZ5AY6w"
+consumer_secret = "QQCtJgaafjmupLMRzNxXKzWoYIdUrn0LsePHNbOk"
+access_token = "23879913-5IdTlih53CXC3JwTj3kJN6ZpvrzBY59Rb5RJpJb4Q"
+access_token_secret = "RFwCqYoZ1W1epTbaNoscGjnJP6uf1Q0wPyt5gMDEVI"
+delimiter = '\t'
--- get_tweets.py
+++ get_tweets.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import csv
+import json
+import os
+import sys
+
+import config
 
 #http://www.tweepy.org/
 import tweepy
 
-#Get your Twitter API credentials and enter them here
-consumer_key = "your_consumer_key"
-consumer_secret = "your_consumer_secret"
-access_key = "your_access_key"
-access_secret = "your_access_secret"
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+csv_ext = '.csv'
+if (config.delimiter == '\t'):
+    csv_ext = '.tsv'
+
+def mkdir(path):
+    if not os.path.exists(path):
+        print "create directory: " + path 
+        os.makedirs(path)
 
 #method to get a user's last 100 tweets
 def get_tweets(username):
+    mkdir(username)
+    #http://tweepy.readthedocs.org/en/v3.1.0/getting_started.html#api
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    #api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    api = tweepy.API(auth)
 
-	#http://tweepy.readthedocs.org/en/v3.1.0/getting_started.html#api
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
+    #set count to however many tweets you want; twitter only allows 200 at once
+    number_of_tweets = 100
 
-	#set count to however many tweets you want; twitter only allows 200 at once
-	number_of_tweets = 100
+    #get tweets
+    tweets = api.user_timeline(screen_name = username,count = number_of_tweets)
 
-	#get tweets
-	tweets = api.user_timeline(screen_name = username,count = number_of_tweets)
 
-	#create array of tweet information: username, tweet id, date/time, text
-	tweets_for_csv = [[username,tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
+    #write to a new csv file from the array of tweets
+    for tweet in tweets :
+        tdir = os.path.join(username, str(tweet.created_at).split(' ')[0])
+        print tweet.created_at
+        mkdir(tdir)
+        with open(os.path.join(tdir, "{0}.json".format(tweet.id_str)), 'w+') as ofile:
+            ofile.write(json.dumps(tweet._json))
+            ofile.close()
 
-	#write to a new csv file from the array of tweets
-	print "writing to {0}_tweets.csv".format(username)
-	with open("{0}_tweets.csv".format(username) , 'w+') as file:
-		writer = csv.writer(file, delimiter='|')
-		writer.writerows(tweets_for_csv)
 
+        with open(tdir + csv_ext, 'a') as csvfile:
+            tweet_for_csv = [tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")]
+            writer = csv.writer(csvfile, delimiter=config.delimiter)
+            writer.writerows([tweet_for_csv])
+            csvfile.close()
 
 #if we're running this as a script
 if __name__ == '__main__':
 
     #get tweets for username passed at command line
     if len(sys.argv) == 2:
+        alias = sys.argv[1]
         get_tweets(sys.argv[1])
     else:
         print "Error: enter one username"
